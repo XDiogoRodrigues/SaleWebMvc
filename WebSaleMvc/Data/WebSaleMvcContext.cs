c6849de1540a733ac82cf22a60b3efd1bed46e09
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-26 12:10:01 -0300

    Declarada os models SalesRecord,Seller e o enum SaleStatus. e feita a segunda migração do MySql

--- WebSaleMvc/Data/WebSaleMvcContext.cs
+++ WebSaleMvc/Data/WebSaleMvcContext.cs
@@ -14,6 +14,8 @@ namespace WebSaleMvc.Data
         {
         }
 
-        public DbSet<WebSaleMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- WebSaleMvc/Migrations/20240126150549_OtherEntities.Designer.cs
+++ WebSaleMvc/Migrations/20240126150549_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSaleMvc.Data;
 
 namespace WebSaleMvc.Migrations
 {
     [DbContext(typeof(WebSaleMvcContext))]
-    partial class WebSaleMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20240126150549_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
