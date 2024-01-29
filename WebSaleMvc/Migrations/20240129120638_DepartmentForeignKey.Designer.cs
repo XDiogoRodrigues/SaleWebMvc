60d22a31059451fed3b5e22954b4fa79dba1f6a1
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-29 09:11:47 -0300

    Chave estrangeira não nula

--- WebSaleMvc/Migrations/20240129120638_DepartmentForeignKey.Designer.cs
+++ WebSaleMvc/Migrations/20240129120638_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSaleMvc.Data;
 
 namespace WebSaleMvc.Migrations
 {
     [DbContext(typeof(WebSaleMvcContext))]
-    partial class WebSaleMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20240129120638_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
