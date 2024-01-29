cb5f0a5052c787c6b934166d64d02b76a6162edd
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-29 12:53:13 -0300

    Retornando pagina de erro de customização

--- WebSaleMvc/Models/ViewsModels/ErrorViewModel.cs
+++ WebSaleMvc/Models/ViewsModels/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace WebSaleMvc.Models.ViewsModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
