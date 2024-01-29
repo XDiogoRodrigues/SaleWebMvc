f88601ef6d31f61381980dfab73a5ab1dd07723d
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-29 09:02:02 -0300

    Criado uma simples formulario

--- WebSaleMvc/Controllers/SellersController.cs
+++ WebSaleMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSaleMvc.Models;
 using WebSaleMvc.Services;
 
 namespace WebSaleMvc.Controllers
@@ -20,6 +21,19 @@ namespace WebSaleMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
