84aab73e8d6e7b34e56be8f68b2a07315f4276a6
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-01-29 12:37:18 -0300

    Atualizar Seller e serviço de exceções personalizadas

--- WebSaleMvc/Services/SellerService.cs
+++ WebSaleMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using WebSaleMvc.Data;
 using WebSaleMvc.Models;
 using Microsoft.EntityFrameworkCore;
+using WebSaleMvc.Services.Exceptions;
 
 namespace WebSaleMvc.Services
 {
@@ -38,6 +39,24 @@ namespace WebSaleMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id)) 
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+          
         }
     }
 }
